513263309fd0177cd833ce8cfb152f13aec70592
Galymzhan1626 <231169@astanait.edu.kz>
2023-12-24 21:19:36 +0600

    added point and shape classes

--- src/models/Point.java
+++ src/models/Point.java
@@ -1,6 +1,21 @@
 package models;
+import java.lang.Math;
+import java.util.ArrayList;
 
 public class Point {
     private double x;
     private double y;
+
+    public Point() {
+        this.x = x;
+        this.y = y;
+    }
+    public ArrayList<Double> distances;
+
+    public void distance(double x, double y){
+        distances.add(Math.sqrt(Math.pow(x-this.x, 2) + Math.pow(y-this.y, 2)));
+    }
+
 }
+
+// toString
--- src/models/Shape.java
+++ src/models/Shape.java
@@ -1,8 +1,26 @@
 package models;
 
+import jdk.internal.access.JavaIOFileDescriptorAccess;
+
 import java.util.ArrayList;
+
 public class Shape {
+    public ArrayList<Point> points;
+    private JavaIOFileDescriptorAccess distances;
 
+    public void addPoint(Point point){
+        points.add(point);
+    }
 
+    public double calculatePerimeter(){
+        int i, perimeter=0;
+        for(i=0;i<points.size();i++){
+            perimeter += distances.get(distances.size() - i);
+        }
+        return perimeter;
+    }
+    public int getLongest(){
 
+        return 0;
+    }
 }
